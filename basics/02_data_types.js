cd3536b56ba1c392a7a4cd98e9ddf8767cb950a6
Pritipatildev <patilpriti66382@gmail.com>
2026-05-27 11:43:04 +0000

    done

---
+++ basics/02_data_types.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") we are using node js not browser
+console.log(3+4);
+
+
+//no of data types
+let name="priti"      //string=<""
+let age=19           //number=>2 to power 53
+let loggedIn=false  //boolean=<(true/false)
+//bigint
+let state=null     //null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//Object
+console.log(typeof"priti");//string
+console.log(typeof null);//gives object
+console.log(typeof undefined);//undefined
+
