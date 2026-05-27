3d68fcb273ac92e6b28c129124b83023f2a5264e
Pritipatildev <patilpriti66382@gmail.com>
2026-05-27 12:11:13 +0000

    conversion of datatypes

---
+++ basics/03_conversion_confusion.js
@@ -0,0 +1,28 @@
+//new_var=data_type(old_var)
+
+let score="priti"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" =>33
+//"33asd"=>NaN
+//true=1/false=0
+
+let isLoggedIn="priti"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+//1=>true;0=>fale
+//""=>false
+//"priti"=>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+console.log(typeof somenumber);
+
+
